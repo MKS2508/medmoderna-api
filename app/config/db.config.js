a95fe31da9dd155782e3df0a4ed982f3d986fd8e
Marcos Asensio <dam02.2020.jesuitas@gmail.com>
2023-03-27 02:56:23 +0200

    s

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  url: "mongodb+srv://root:root@cluster0.ninww.mongodb.net/medmoderna-db2"
+
+  url: "mongodb://medModernaAdmin:kiarakiara@2.223.69.193:27017/medmoderna-db"
+
+
 };
