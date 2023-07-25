66de109894b90b7b1298577431b1a9f449d0f813
Marcos Asensio <dam02.2020.jesuitas@gmail.com>
2023-07-25 19:33:48 +0200

    Revert "test https"
    This reverts commit b6d55949424976e567e37d23123460f85bd35978.

--- server.js
+++ server.js
@@ -4,15 +4,19 @@ const morgan = require("morgan");
 const compression = require("compression");
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-const fs = require('fs');
-const https = require('https');
 require('dotenv').config();
+
+
+// ... tus otros middlewares (bodyParser, etc.)cccc
+
+
+
 
 const app = express();
 const initialSetup = require("./app/libs/initialSetup")
 
 const corsOptions = {
-    //origin: '*',
+  //origin: '*',
     origin: ["https://medmoderna.vercel.app","http://www.medicinamodernagrow.shop", "https://www.medicinamodernagrow.shop","http://127.0.0.1:3000", "http://192.168.1.150:3000", "http://93.93.118.47:80", "https://93.93.118.47:443", "http://93.93.118.47"],
     credentials: true
 };
@@ -20,26 +24,32 @@ initialSetup.createProductCategories().then();
 app.use(cookieParser());
 app.use(cors(corsOptions));
 app.use(morgan('dev'));
-
+// parse requests of content-type - application/json
 app.use(bodyParser.json({ limit: '100mb' }));
+
+// parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const db = require("./app/models");
 db.mongoose
-    .connect(db.url, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(() => {
-        console.log("Connected to the database!");
-    })
-    .catch(err => {
-        console.log("Cannot connect to the database!", err);
-        process.exit();
-    });
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
 
+
+// Configura el middleware para servir archivos estáticos comprimidos
 app.use(compression());
 
+
+// Configura el middleware para servir archivos estáticos y mostrar un mensaje de error si algo sale mal
 app.use("/api/static", express.static("public", {
     fallthrough: false,
     setHeaders: function (res, path, stat) {
@@ -50,8 +60,10 @@ app.use("/api/static", express.static("public", {
     res.status(404).send("Archivo no encontrado");
 });
 
+// ... tus rutas (app.get, require, etc.)
+// simple route
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome to medicina moderna REST-API application." });
+  res.json({ message: "Welcome to medicina moderna REST-API application." });
 });
 
 require("./app/routes/product.routes")(app);
@@ -59,13 +71,6 @@ require("./app/routes/auth.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 5000;
-
-// Configura HTTPS y las rutas SSL
-const httpsOptions = {
-    key: fs.readFileSync('./medicinamodernagrow.cer'),
-    cert: fs.readFileSync('./_.medicinamodernagrow.key')
-}
-
-const server = https.createServer(httpsOptions, app).listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
