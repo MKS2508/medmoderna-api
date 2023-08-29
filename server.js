3c3f6dcb07cd0d36c4e5eea65a4db01a740f0304
Marcos Asensio <dam02.2020.jesuitas@gmail.com>
2023-08-29 14:31:51 +0200

    test

--- server.js
+++ server.js
@@ -78,8 +78,6 @@ const PORT = process.env.PORT || 8080;
 
 
 
-const server = createServer(app).listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
-});
+const server = createServer(app);
 
 module.exports=  server;
